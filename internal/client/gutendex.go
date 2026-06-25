cd39eb9cb8315e31029ba92bf024bb99fcb3085a
xThirteenIII <giuseppe.richetti@gmail.com>
2026-06-25 19:18:40 +0200

    feat: add SearchBooksByID function

--- internal/client/gutendex.go
+++ internal/client/gutendex.go
@@ -2,8 +2,14 @@ package client
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/url"
+	"strconv"
+)
+
+var (
+	ErrNoMatch = errors.New("no id match")
 )
 
 type Author struct {
@@ -28,8 +34,17 @@ type Gutendex struct {
 	Results []Book `json:"results"`
 }
 
+// SearchBooksByString uses the passed parameter as the query parameter for gutendex API use.
+// It returns a slice of Books, built with the results from the search API.
+// It accepts an empty string, since Gutendex API accepts it.
+
+// Error returned as it is, since it's handled by the search books handler.
 func SearchBooksByString(s string) ([]Book, error) {
+
+	// Parse URL
 	url := "https://gutendex.com/books/?search=" + url.QueryEscape(s)
+
+	// Create http Client
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -37,11 +52,14 @@ func SearchBooksByString(s string) ([]Book, error) {
 	}
 	req.Header.Add("Content-Type", "application/json")
 
+	// Send request
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	// Decode response body, save it into Results []Books and return it.
 	// g.Results returns 32 Books at most, Gutendex rule.
 	var g Gutendex
 
@@ -49,4 +67,47 @@ func SearchBooksByString(s string) ([]Book, error) {
 		return nil, err
 	}
 	return g.Results, nil
+}
+
+// SearchBooksByID queries Gutendex API to check if a Book exists given a specific, single ID.
+// This is used for fast checking if a POST /review request body matches any Book in the Gutendex database.
+
+// Returns an  if there is no match.
+// Chose returning an error over a bool to differentiate No Match from other errors.
+func SearchBooksByID(id int) error {
+
+	strID := strconv.Itoa(id)
+	// Parse URL
+	url := "https://gutendex.com/books/?ids=" + url.QueryEscape(strID)
+
+	// Create http Client
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send request
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Decode response body, save it into Results []Books and return it.
+	// g.Results returns 32 Books at most, Gutendex rule.
+	var g Gutendex
+
+	if err := json.NewDecoder(resp.Body).Decode(&g); err != nil {
+		return err
+	}
+
+	// len(slice) == 0 better than slice == nil.
+	// It covers both nil and empty slice.
+	//
+	if len(g.Results) == 0 {
+		return ErrNoMatch
+	}
+	return nil
 }
