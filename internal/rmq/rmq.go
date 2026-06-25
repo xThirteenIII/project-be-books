2e8a0307c4e57b7b193d11d172f1d532cfacbd78
xThirteenIII <giuseppe.richetti@gmail.com>
2026-06-25 19:19:46 +0200

    feat: add connection to RabbitMQ

---
+++ internal/rmq/rmq.go
@@ -0,0 +1,43 @@
+package rmq
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var connAttempts = 10
+
+func AttemptConnect() (*amqp.Connection, error) {
+	rabbitURL := os.Getenv("RABBITMQ_URL")
+	if rabbitURL == "" {
+		rabbitURL = "amqp://user:password@localhost:5672/"
+	}
+	var err error
+	var conn *amqp.Connection
+	for connAttempts > 0 {
+		conn, err = amqp.Dial(rabbitURL)
+		if err == nil {
+			break
+		}
+		log.Printf("RabbitMQ is trying to connect, attempts left: %d\n", connAttempts)
+		time.Sleep(5 * time.Second)
+		connAttempts--
+
+	}
+	if err != nil {
+		return nil, fmt.Errorf("amqp.Dial: %v\n", err)
+	}
+	//defer conn.Close()
+	log.Printf("Connection to RabbiMQ successful.\n")
+	/*
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		<-signalChan
+		log.Printf("Exiting gracefully...\n")
+	*/
+	return conn, nil
+}
