21b15740d429fbe930e462c2e5e3fb4ac6cd2ff2
xThirteenIII <giuseppe.richetti@gmail.com>
2026-06-26 16:32:20 +0200

    chore: use new gutendex package name

--- internal/handler/handler_search_books.go
+++ internal/handler/handler_search_books.go
@@ -1,17 +1,19 @@
 package handler
 
 import (
-	"books/internal/client"
+	"books/internal/gutendex"
 	"books/internal/utils"
 	"net/http"
 )
 
+// SearchBooksByKeywords queries Gutendex GET API to search books given a string.
+// Don't need a service for this since the logic is small.
 func SearchBooksByKeywords(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query().Get("q")
-	// Not checking for empty q since Gutendex method allows it.
+	// Not checking for empty q since Gutendex API allows it.
 	// It returns first 32 books.
 
-	books, err := client.SearchBooksByString(q)
+	books, err := gutendex.SearchBooksByString(q)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusInternalServerError, "failed to fetch books", err)
 		return
