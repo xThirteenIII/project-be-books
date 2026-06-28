999db317552d9401494f85e2ac84376578c38343
xThirteenIII <giuseppe.richetti@gmail.com>
2026-06-28 20:42:32 +0200

    refactor: match method signatures

--- internal/repo/maria.go
+++ internal/repo/maria.go
@@ -4,6 +4,8 @@ import (
 	"books/internal/model"
 	"context"
 	"database/sql"
+	"errors"
+	"time"
 )
 
 // Concrete implementation of ReviewRepo
@@ -13,6 +15,8 @@ type MariaDBReviewRepo struct {
 
 // Verify implementation at compile time.
 // var _ ReviewRepo = &MariaDBReviewRepo{}
+
+var ErrNotFound = errors.New("review not found")
 
 func NewMariaDBReviewRepository(db *sql.DB) *MariaDBReviewRepo {
 	return &MariaDBReviewRepo{db: db}
@@ -34,20 +38,66 @@ func (r *MariaDBReviewRepo) Insert(ctx context.Context, review model.Review) err
 	return err
 }
 
-func (r *MariaDBReviewRepo) GetByID(ctx context.Context, id string) error {
-	query := `SELECT FROM reviews id
-              VALUES ?`
+func (r *MariaDBReviewRepo) GetByID(ctx context.Context, id string) (model.Review, error) {
+	query := `SELECT id, book_id, score, review_text, status, book_title, authors, cover_url, created_at, updated_at
+			  FROM reviews
+			  WHERE id = ?`
 
-	_, err := r.db.ExecContext(ctx, query, id)
-	return err
+	var review model.Review
+
+	// Use of NullString resolves the problematic scan of NULL fields into strings.
+	// These fields can be NULL while review status is pending.
+	var bookTitle sql.NullString
+	var authors sql.NullString
+	var coverURL sql.NullString
+
+	err := r.db.QueryRowContext(ctx, query, id).Scan(
+		&review.ID,
+		&review.BookID,
+		&review.Score,
+		&review.ReviewText,
+		&review.Status,
+		&bookTitle,
+		&authors,
+		&coverURL,
+		&review.CreatedAt,
+		&review.UpdatedAt,
+	)
+	// If there's no reviews corresponding to given ID, return ErrNotFound.
+	if errors.Is(err, sql.ErrNoRows) {
+		return model.Review{}, ErrNotFound
+	}
+	if err != nil {
+		return model.Review{}, err
+	}
+
+	// NULL fields become empty strings.
+	review.BookTitle = bookTitle.String
+	review.Authors = authors.String
+	review.CoverURL = coverURL.String
+
+	return review, nil
 }
 
 func (r *MariaDBReviewRepo) Delete(ctx context.Context, id string) error {
-	query := `SELECT FROM reviews id
-              VALUES ?`
+	query := `DELETE FROM reviews WHERE id = ?`
 
-	_, err := r.db.ExecContext(ctx, query, id)
-	return err
+	res, err := r.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// If no rows were updated, review id does not exist.
+	if rowsAffected == 0 {
+		return ErrNotFound
+	}
+
+	return nil
 }
 
 func (r *MariaDBReviewRepo) Update(ctx context.Context, review model.Review) error {
@@ -55,7 +105,7 @@ func (r *MariaDBReviewRepo) Update(ctx context.Context, review model.Review) err
 			  SET book_title = ?, authors = ?, cover_url = ?, status = ?, updated_at = ?
 			  WHERE id = ?`
 
-	_, err := r.db.ExecContext(ctx, query,
+	res, err := r.db.ExecContext(ctx, query,
 		review.BookTitle,
 		review.Authors,
 		review.CoverURL,
@@ -63,5 +113,40 @@ func (r *MariaDBReviewRepo) Update(ctx context.Context, review model.Review) err
 		review.UpdatedAt,
 		review.ID,
 	)
-	return err
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrNotFound
+	}
+
+	return nil
+}
+
+// UpdateReviewContent updates score and review_text only.
+// We avoid using Update() method for both PUT and POST endpoints.
+func (r *MariaDBReviewRepo) UpdateReviewContent(ctx context.Context, id string, reviewText string, score int) error {
+	query := `UPDATE reviews
+			  SET review_text = ?, score = ?, updated_at = ?
+			  WHERE id = ?`
+
+	res, err := r.db.ExecContext(ctx, query, reviewText, score, time.Now(), id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrNotFound
+	}
+
+	return nil
 }
